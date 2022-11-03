680f6cd2c7f7959e485c4fccf264732148c86627
raphaelrl86 <raphael.lacerda@gmail.com>
2022-11-03 19:29:22 -0300

    2 commit

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
-const prompt = require('prompt-sync')();
+// const prompt = require('prompt-sync')();
 
+const button = document.getElementById("start")
+const failureLetters = document.getElementById("wrong")
+const choice = document.querySelector("input")
+const knightsLetter = document.querySelector(".secret-word-container")
 
 // 1. Criar array com as palavras
 const words = ["Jabu", "Ichi", "Nachi", "Ban", "Geki", "June", "Marin", "Shaina", "Misty", "Moses", "Babel", "Asterion", "Jamian", "Algol", "Capella", "Algethi", "Dante", "Dio", "Sirius", "Ptolemeu", "Daidalos", "Orpheu", "Suikyo", "Mu", "Aldebaran", "Saga", "Kanon", "Deathmask", "Aiolia", "Shaka", "Dohko", "Milo", "Aiolos", "Shura", "Camus", "Aphrodite", "Siegfried", "Hagen", "Thor", "Alberich", "Fenrir", "Syd", "Bud", "Mime", "Seiya", "Shun", "Shiryu", "Hyoga", "Ikki"]
@@ -15,65 +19,57 @@ const secretWord = words[randomWord].toUpperCase()
 const correctLetters = []
 const wrongLetters = []
 
-document.addEventListener("keydown", (event) => {
-    const code = event.keyCode
-    if(isLetters(code)) {
-        const letters = event.key
-    }
-}
-)
-
-
-
 function hasTheLetter (letter, secretWord) {
-    let letterFound = false
-    for (let i = 0; i < secretWord.length; i++) {
-        if (letter === secretWord[i]) {
-            console.log(letter === secretWord[i])
-            letterFound = true 
-            correctLetters[i] = letter
-            correctAttempts++
+    // let letterFound = false
+    letterFound = secretWord.includes(letter)
+    if (letterFound) {
+        for (let i = 0; i < secretWord.length; i++) {
+            if (letter === secretWord[i]) {
+                correctLetters[i] = letter
+                correctAttempts++
+                knightsLetter.innerHTML = correctLetters.join("")
+                endGame()
+            } else {
+                knightsLetter.innerHTML += `<span>_</span>`
+            }
         }
     }
+    
     if(!letterFound){
         wrongLetters.push(letter);
         failures++
+        failureLetters.innerText = wrongLetters.join("")
+        endGame()
     }
-    return letterFound
+    // return letterFound
 }
 
 function playGame()  {
-    
-    do { 
-        const letter = prompt('type a letter: ').toUpperCase();
+        let letter = choice.value.toUpperCase() 
         hasTheLetter(letter, secretWord)
-        console.log("correctletters")
-        console.log(correctLetters)
-        console.log("wrongLetters")
-        console.log(wrongLetters)
-    } while (failures < MAX_ERRORS && correctAttempts < secretWord.length)
-
-
+        choice.value = ""
 }
 
-playGame()
-
-function showWrongLetters () {
-    const div = document.querySelector(".wrong-letters")
-    div.innerHTML = "<h3>Letras erradas</h3>"
-    wrongLetters.forEach((letter) => {
-        div.innerHTML += `<span>${letter}</span>`
-    });
+function endGame() {
+    if (failures === 6 || knightsLetter.innerHTML.length === secretWord.length) {
+        console.log("Fim de jogo")
+    } 
 }
 
+button.addEventListener('click', playGame)
+    // let letter = prompt('DIGITE UMA LETRA');
+//     playGame(letter)
+//   }
+// function showWrongLetters () {
+//     }
+
+// function showCorrectLetters() {
+
+// }
+
+// function desenharForca (){
+
+// }
 
 
-
-// 3. Separar as letras da palavra
-// 4. Fazer um teclado virtual
-// 5. Colocar as letras usadas e erradas dentro de um array ou inutilizar o botão com a letra? 
-// 6. Exibir as letras certas nas posições corretas
-// 7. Para cada letra errada exibir um pedaço do bonequinho
-// 8. Exibir mensagem de vitória ou derrota
-// 9. Começar o jogo novamente
 
