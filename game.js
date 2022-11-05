fa137d85e38ad97ea33021ba1b41b1a6adc5bed3
raphaelrl86 <raphael.lacerda@gmail.com>
2022-11-04 22:50:53 -0300

    4 commit

--- game.js
+++ game.js
@@ -1,14 +1,19 @@
-// const prompt = require('prompt-sync')();
-
 const button = document.getElementById("start")
 const failureLetters = document.getElementById("wrong")
 const choice = document.querySelector("input")
 const knightsLetter = document.querySelector(".secret-word-container")
 const endGameMessage = document.getElementById("end-game-message")
+const winAudio = document.getElementById("win-audio")
+const defeatAudio = document.getElementById("defeat-audio")
+const bgAudio = document.getElementById("bg-audio")
+bgAudio.volume = .25
+winAudio.volume = .25
+defeatAudio.volume = .25
+const attempts = document.getElementById("attempts")
 
-// 1. Criar array com as palavras
+
 const words = ["Jabu", "Ichi", "Nachi", "Ban", "Geki", "June", "Marin", "Shaina", "Misty", "Moses", "Babel", "Asterion", "Jamian", "Algol", "Capella", "Algethi", "Dante", "Dio", "Sirius", "Ptolemeu", "Daidalos", "Orpheu", "Suikyo", "Mu", "Aldebaran", "Saga", "Kanon", "Deathmask", "Aiolia", "Shaka", "Dohko", "Milo", "Aiolos", "Shura", "Camus", "Aphrodite", "Siegfried", "Hagen", "Thor", "Alberich", "Fenrir", "Syd", "Bud", "Mime", "Seiya", "Shun", "Shiryu", "Hyoga", "Ikki"]
-// 2. Sortear uma palavra aleatória dentro desse array
+
 
 const randomWord = Math.floor(Math.random()*words.length)
 console.log(words[randomWord])
@@ -16,19 +21,18 @@ const MAX_ERRORS = 6
 let correctAttempts = 0
 let failures = 0
 const secretWord = words[randomWord].toUpperCase()
-
+bgAudio.play()
 const correctLetters = []
 const wrongLetters = []
 
 function hasTheLetter (letter, secretWord) {
-    // let letterFound = false
     letterFound = secretWord.includes(letter)
     if (letterFound) {
         for (let i = 0; i < secretWord.length; i++) {
             if (letter === secretWord[i]) {
                 correctLetters[i] = letter
                 correctAttempts++
-                // knightsLetter.innerHTML = correctLetters.join(" ")
+                
                 endGame()
             } else if (!correctLetters[i]){ 
                 correctLetters[i] = "_"
@@ -41,9 +45,10 @@ function hasTheLetter (letter, secretWord) {
         wrongLetters.push(letter);
         failures++
         failureLetters.innerText = wrongLetters.join("")
+        attempts.innerHTML = `Você tem ${MAX_ERRORS - failures} tentativas restantes`
         endGame()
     }
-    // return letterFound
+  
 }
 
 function playGame()  {
@@ -55,25 +60,15 @@ function playGame()  {
 function endGame() {
     if (failures === 6) {
         endGameMessage.innerHTML = "Você perdeu!!!"
+        defeatAudio.play()
     } else if (correctLetters.join("") === secretWord) {
         endGameMessage.innerHTML = "Parabéns, você acertou!!!"
+        winAudio.play()
     }
   }
 
 button.addEventListener('click', playGame)
-    // let letter = prompt('DIGITE UMA LETRA');
-//     playGame(letter)
-//   }
-// function showWrongLetters () {
-//     }
-
-// function showCorrectLetters() {
-
-// }
-
-// function desenharForca (){
-
-// }
+    
 
 
 
